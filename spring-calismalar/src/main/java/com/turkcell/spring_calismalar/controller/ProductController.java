45901befe145d833bdd514f466812bcd79a5f3f9
Osman Bulbul <obulbul713@gmail.com>
2026-04-13 18:16:10 +0300

    -ilk asama olarak GET POST metodlarinin nasil kullanildigini gordum. -@RequestBody ve @RequestParam arasindaki fark ve bu farkin getirdigi kullanim gereklilikleri nelerdir onlari gordum. -DTO'nun ne oldugu ne amacla kullanildigi ve nasil kullanildigini gordum.

---
+++ spring-calismalar/src/main/java/com/turkcell/spring_calismalar/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.turkcell.spring_calismalar.controller;
+
+import com.turkcell.spring_calismalar.dto.ProductRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @GetMapping("/test")
+    public String deneme(){
+        return "Test icin gonderildi.";
+    }
+
+    
+    // @PostMapping("/add")
+    // public String addProduct(@RequestParam String productName,@RequestParam double productPrice,@RequestParam int productNumber){
+    //     return productName+" isimli urun eklendi. Fiyat: "+productPrice+" TL.Adet :"+productNumber;
+    // }
+
+    @PostMapping("/add")
+    public String addProduct(@RequestBody ProductRequest request){
+        return request.productName+" isimli urun eklendi. Fiyat: "+request.productPrice+" TL. Adet :"+request.productNumber;
+    }
+
+
+
+}
---
+++ spring-calismalar/src/main/java/com/turkcell/spring_calismalar/dto/ProductRequest.java
@@ -0,0 +1,10 @@
+package com.turkcell.spring_calismalar.dto;
+
+public class ProductRequest {
+
+    //Springin JSON'i buraya dokebilmesi icin parametreler public veya getter/setter metotlarini kullnamamiz gerekiyor. Teknik bir bilgi olarak, JSON verilerini Java nesnelerine dönüştürmek için kullanılan kütüphaneler (örneğin, Jackson) genellikle sınıfın alanlarına erişim sağlar. Bu nedenle, JSON verilerini doğru şekilde eşleştirebilmek için bu alanların public olması veya getter/setter metotlarının bulunması gerekmektedir. Aksi takdirde, JSON verileri bu alanlara erişemez ve dönüşüm işlemi başarısız olabilir. 
+
+    public String productName;
+    public double productPrice;
+    public int productNumber;
+}
