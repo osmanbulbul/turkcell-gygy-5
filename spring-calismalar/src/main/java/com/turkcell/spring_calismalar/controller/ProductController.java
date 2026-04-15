3e2aa0f74585aff78a6355467730d8ff057077a3
Osman Bulbul <obulbul713@gmail.com>
2026-04-15 18:21:14 +0300

    ResponseEntity kullanimini burada kavradik. 200 OK ,201 Created,400 BAD REQUEST gorduk. Bunlarin calisma durumlari ve sureci PostMan uzerinden de basarili sekilde goruldu.

--- spring-calismalar/src/main/java/com/turkcell/spring_calismalar/controller/ProductController.java
+++ spring-calismalar/src/main/java/com/turkcell/spring_calismalar/controller/ProductController.java
@@ -2,7 +2,7 @@ package com.turkcell.spring_calismalar.controller;
 
 import com.turkcell.spring_calismalar.dto.ProductRequest;
 import org.springframework.web.bind.annotation.RequestMapping;
-// import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,20 +30,45 @@ public class ProductController {
     //     return request.productName+" isimli urun eklendi. Fiyat: "+request.productPrice+" TL. Adet :"+request.productNumber;
     // }
 
-    @PostMapping("/add")
-    public ResponseEntity<String> addProduct(@RequestBody ProductRequest request){
+    // @PostMapping("/add")
+    // public ResponseEntity<String> addProduct(@RequestBody ProductRequest request){
 
-        // basit bir kontrol (is mantigi/ business logic baslangici)
-        if(request.productName==null||request.productName.isEmpty()){
-            //hatali istek olma durumu
-            return ResponseEntity
-                    .status(HttpStatus.BAD_REQUEST)
-                    .body("hatali durum, Urun ismi bos olamaz.");
+    //     // basit bir kontrol (is mantigi/ business logic baslangici)
+    //     if(request.productName==null||request.productName.isEmpty()){
+    //         //hatali istek olma durumu
+    //         return ResponseEntity
+    //                 .status(HttpStatus.BAD_REQUEST)
+    //                 .body("hatali durum, Urun ismi bos olamaz.");
+    //     }
+
+    //     //basarili kayit durumu 201 Created
+    //     String mesaj=request.productName+" Basaryia sisteme kaydedildi.";
+    //     return new ResponseEntity<>(mesaj,HttpStatus.CREATED);
+    // }
+
+    @GetMapping("/not-sorgula")
+    public ResponseEntity<String> getSinavSonucu(@RequestParam int not) {
+
+        if (not < 0 || not > 100) {
+            // Durum: Geçersiz veri gönderildi
+            // Etiket: 400 Bad Request
+            return new ResponseEntity<>("Hata: Not 0-100 arasında olmalıdır!", HttpStatus.BAD_REQUEST);
         }
 
-        //basarili kayit durumu 201 Created
-        String mesaj=request.productName+" Basaryia sisteme kaydedildi.";
-        return new ResponseEntity<>(mesaj,HttpStatus.CREATED);
+        if (not >= 50) {
+            // Durum: Başarılı ve Geçti
+            // Etiket: 200 OK
+            return new ResponseEntity<>("Tebrikler, dersten geçtiniz!", HttpStatus.OK);
+        } else {
+            // Durum: Başarılı ama Kaldı
+            // Etiket: 200 OK (Çünkü işlem teknik olarak başarılı, sadece sonuç olumsuz)
+            return new ResponseEntity<>("Maalesef dersten kaldınız.", HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/urun-goster")
+    public String getUrun(@RequestBody ProductRequest request){
+        return request.productName+" isimli urun gosterildi. \nFiyat: "+request.productPrice+ " Tl.\nAdet: "+request.productNumber; 
     }
 
 
