5cf08b37dd34c8704d49c7f5e3af0aa2810c80ce
Osman Bulbul <obulbul713@gmail.com>
2026-04-27 16:05:17 +0300

    .

--- spring-starter/src/main/java/com/turkcell/spring_starter/repository/CategoryRepository.java
+++ spring-starter/src/main/java/com/turkcell/spring_starter/repository/CategoryRepository.java
@@ -3,7 +3,11 @@ package com.turkcell.spring_starter.repository;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.turkcell.spring_starter.entity.Category;
-public class CategoryRepository {
+import org.springframework.stereotype.Repository;
 
+import com.turkcell.spring_starter.entity.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, UUID>
+{
 }
