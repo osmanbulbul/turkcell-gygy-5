f35f9c55ae3e26912b392a008364f8511d6d55f8
Osman Bulbul <obulbul713@gmail.com>
2026-04-27 17:18:33 +0300

    PRODUCT RESPONSE AND REQUEST -listproductRespnse

---
+++ spring-starter/src/main/java/com/turkcell/spring_starter/dto/CreateProductRequest.java
@@ -0,0 +1,30 @@
+package com.turkcell.spring_starter.dto;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public class CreateProductRequest {
+
+    @NotBlank(message = "Ürün adı boş olamaz")
+    @Size(max = 100)
+    private String name;
+
+    @Size(max = 500)
+    private String description;
+
+    // Hangi kategoriye ait olduğunu ID ile alıyoruz
+    @NotNull(message = "Kategori ID zorunludur")
+    private UUID categoryId;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    public UUID getCategoryId() { return categoryId; }
+    public void setCategoryId(UUID categoryId) { this.categoryId = categoryId; }
+}
